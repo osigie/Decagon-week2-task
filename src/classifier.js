07b4075f068c606d0ccdccff11be6375b43bbf1e
usser.email <user@MacBook-Air-de-user.local>
2022-02-23 15:02:33 +0100

    final changes

--- src/classifier.js
+++ src/classifier.js
@@ -1,3 +1,90 @@
-function classifier(input) {}
+import { convertCompilerOptionsFromJson } from "typescript";
+
+function classifier(input) {
+  // clone the input
+  let inputClone = [...input];
+  //check if the input is an array
+  if (!Array.isArray(input)) {
+    throw Error("Invalid inputClone");
+  }
+
+  // check if the input has one element
+  if (input.length == 0) {
+    return { noOfGroups: 0 };
+  }
+  let sample = [];
+
+  // function to calculate age
+  const ageCal = (date) => {
+    return new Date(2019, 0, 1).getFullYear() - new Date(date).getFullYear();
+  };
+
+  //adding calculated age to the given input
+  inputClone.forEach((curr) => {
+    const name = curr.name;
+    const dob = curr.dob;
+    const regNo = curr.regNo;
+    const age = ageCal(curr.dob);
+
+    sample.push({ name: name, dob: dob, regNo: regNo, age: age });
+  });
+
+  // sort the input with the calculated age
+  const sortedBy = sample.sort(function (a, b) {
+    return a.age - b.age;
+  });
+
+  // sorting the input based on difference of age <= 50
+
+  let sections = [sortedBy[0]]; // temporary array and puting first element as case study
+  let new_arr = []; // the permanet array to receive the groups
+
+  for (let i = 1; i < sortedBy.length; i++) {
+    if (sortedBy[i].age - sections[0].age <= 5 && sections.length <= 2) {
+      sections.push(sortedBy[i]);
+    } else {
+      new_arr.push(sections);
+      sections = [];
+      sections.push(sortedBy[i]);
+    }
+  }
+  // when we have just one element in the temporary array which is lesser than 3, we still have to push to permanent array
+
+  if (sections.length) {
+    new_arr.push(sections);
+  }
+
+  // using object leterals to create the final housing object
+  let result = {};
+
+  // creating number of group with the total number of sub arrays in the total array
+  result.noOfGroups = new_arr.length;
+
+  // creating oldest, sum, regNos of the students
+  const eachGroup = new_arr.map((current) => {
+    return {
+      members: current.map((each) => ({
+        name: each.name,
+        age: each.age,
+        dob: each.dob,
+        regNo: each.regNo,
+      })),
+      oldest: current[current.length - 1].age,
+      sum: current.reduce((accumulator, current) => {
+        return accumulator + current.age;
+      }, 0),
+      regNos: current.map((a) => Number(a.regNo)).sort((a, b) => a - b),
+    };
+  });
+  // console.log(eachGroup)
+
+  // made the object dynamic
+  eachGroup.forEach((each, index) => {
+    const groupNum = `group${index + 1}`;
+    result = { ...result, [groupNum]: each };
+  });
+
+  return result;
+}
 
 export default classifier;
--- src/duplicate-transactions.js
+++ src/duplicate-transactions.js
@@ -1,3 +1,76 @@
-function findDuplicateTransactions(transactions) {}
+import input from "../fixtures/input/edge-input";
+
+function findDuplicateTransactions(transactions) {
+  // clone the initial input
+  let cloneTransactions = [...transactions];
+
+  // check if the input is an arry
+  if (!Array.isArray(transactions)) {
+    throw Error;
+  }
+  // check if the input length is equal to one
+  if (input.length == 1 && input.length) {
+    return [];
+  }
+
+  //first, i am sorting transactions that have the same properties apart from time
+  let firstCat = [];
+
+  // sorting with id
+  cloneTransactions.sort((a, b) => {
+    return a.id - b.id;
+  });
+
+  // initializing temporary array with first element
+  let tem = [cloneTransactions[0]];
+
+  for (let i = 1; i < cloneTransactions.length; i++) {
+    if (
+      cloneTransactions[i].sourceAccount === tem[0].sourceAccount &&
+      cloneTransactions[i].targetAccount === tem[0].targetAccount &&
+      cloneTransactions[i].category === tem[0].category &&
+      cloneTransactions[i].amount === tem[0].amount
+    ) {
+      tem.push(cloneTransactions[i]);
+    } else {
+      // console.log(tem)
+      firstCat.push(tem);
+      tem = [];
+      tem.push(cloneTransactions[i]);
+    }
+  }
+  // pushed the last element in the temporary array to the permanent array
+
+  if (firstCat.length) {
+    firstCat.push(tem);
+  }
+  // after grouping, i check for the sub arrays with length > 1
+
+  let realUn = [];
+  for (const each of firstCat) {
+    if (each.length > 1) {
+      realUn.push(each);
+    }
+  }
+
+  console.log(realUn);
+
+  //sorting with time
+  let realResult = [];
+  let tempResult = [realUn[0]];
+
+  for (let i = 0; i < realUn.length; i++) {
+    if (calTime(realUn[i].time) - calTime(tempResult[0].time) < 1) {
+      tempResult.push(realUn[i]);
+    } else {
+      realResult.push(tempResult);
+      tempResult = [];
+      tempResult.push(realUn[i]);
+    }
+  }
+  return realResult;
+}
+
+
 
 export default findDuplicateTransactions;
